0a7ad27e6fb82803c5fc24fea4bdda1811a50ad5
VaiLogoCaramba <150534279+VaiLogoCaramba@users.noreply.github.com>
2024-04-02 13:51:46 -0300

    Update FlashingState.hx

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -13,16 +13,19 @@ class FlashingState extends MusicBeatState
 	var warnText:FlxText;
 	override function create()
 	{
+                #if android
+		addVirtualPad(NONE, A_B);
+		#end
 		super.create();
 
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
+			"Feliz Páscoa FNF BR Port By:Sinix Skr!\n
 			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
+			Press A to disable them now or go to Options Menu.\n
+			Press B to ignore this message.\n
 			You've been warned!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
